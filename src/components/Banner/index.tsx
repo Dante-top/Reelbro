b32ba03383f12f8c2c0cc560af25f78e8177b7ef
-g <dantetopdev@gmail.com>
2025-03-12 19:19:45 -0400

    feat:update-connect-wallet-to-big-box

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -4,7 +4,7 @@ import SocialButton from "../SocialButton";
 
 const Banner: React.FC = () => {
   return (
-    <div className={`${styles.banner} container-lg`}>
+    <div className={`${styles.banner}`}>
       <div className={styles.banner_subtitle}>
         <h2>WHERE SPORTS. CHARITY & CRYPTO COLLIDE</h2>
       </div>
@@ -12,11 +12,17 @@ const Banner: React.FC = () => {
         <div className={`${styles.banner_social}`}>
           <SocialButton />
         </div>
-        <div className={styles.banner_btnWallet}>
-          <button className={`${styles.walletConnect}`} onClick={() => {}}>
-            CONNECT&nbsp;WALLET
-          </button>
-        </div>
+      </div>
+      <div className={styles.banner_btnWallet}>
+        <h2>MINT REELBRO TOKEN NOW!</h2>
+        <p>
+          You can now claim your $FLOCK tokens. Plus, stake your tokens to earn
+          rewards! Add 0xb33D999469a7e6b9EbC25A3a05248287b855eD46 to your wallet
+          to see your $FLOCK.
+        </p>
+        <button className={`${styles.walletConnect}`} onClick={() => {}}>
+          CONNECT&nbsp;WALLET
+        </button>
       </div>
     </div>
   );
